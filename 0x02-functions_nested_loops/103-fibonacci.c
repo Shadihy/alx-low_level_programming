05dc000c3c219ae87c15ed6015c72fae1f878aaa
Shadihy <shadnjoroge@gmail.com>
2023-06-20 06:44:18 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,23 +5,19 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int a = 0, b = 1;
+	int a = 0, b = 1, next = 0;
 	int sum = 0;
 
-	while (a < 4000000)
+	while (next < 4000000)
 	{
-		if (a % 2 == 0)
-		{
-			sum += a;
-		}
-		int sum = a + b;
-
+		next = a + b;
 		a = b;
-		b = sum;
+		b = next;
+		if (next % 2 == 0)
+			sum += next;
 	}
-	printf("%d", sum);
+	printf("%d\n", sum);
 	return (0);
 }
