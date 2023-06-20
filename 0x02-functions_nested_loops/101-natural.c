e1afcd0bd0a493c39cd89b1805cf462f0bcf9279
Shadihy <shadnjoroge@gmail.com>
2023-06-20 04:28:41 -0700

    All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main() 
+/**
+ * main - main block
+ * Description: Print the sum of all multiples of 3 or 5 below 1024.
+ * Return: 0
+ */
+int main(void)
 {
-    int limit = 1024;
-    int sum = 0;
+	int limit = 1024;
+	int sum = 0;
 
-    for (int i = 1; i < limit; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
-
-    printf("The sum of multiples of 3 or 5 below %d is: %d\n", limit, sum);
-
-    return 0;
+	for (int i = 1; i < limit; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\", sum);
+	return 0;
 }
