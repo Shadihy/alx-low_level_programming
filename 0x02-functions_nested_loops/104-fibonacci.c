e440b77d72be46ae602f33cca0d133575a2df9ea
Shadihy <shadnjoroge@gmail.com>
2023-06-20 07:02:24 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,20 +8,55 @@
  */
 int main(void)
 {
-	int count = 98;
-	long int fib1 = 1, fib2 = 2;
+	int length = 0;
 
-	printf("%ld %ld ", fib1, fib2);
-
-	for (int i = 3; i <= count; i++)
+	if (!num)
 	{
-		long int fib3 = fib1 + fib2;
-
-		printf("%ld ", fib3);
-		fib1 = fib2;
-		fib2 = fib3;
+		return (1);
 	}
 
-	printf("\n");
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+	return (length);
+}
+/**
+ *  main - prints the first 98 fibonaci sequences
+ *  Return: 0
+ */
+
+int main(void)
+{
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
+	short int i = 1, initial0s;
+
+	while (i <= 98)
+	{
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = len(mx) - 1 - len(f1);
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%i", 0);
+			initial0s--;
+		}
+		printf("%lu", f1);
+
+		tmp = (f1 + f2) % mx;
+		tmpo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = tmp;
+		f2o = tmpo;
+
+		if (i != 98)
+			printf(", ");
+		else
+			printf("\n");
+		i++;
+	}
 	return (0);
 }
