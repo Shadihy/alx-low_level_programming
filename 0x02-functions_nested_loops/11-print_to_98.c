b4fc20521db680aa6dcc3727ced2eeedff754749
Shadihy <shadnjoroge@gmail.com>
2023-06-20 03:45:11 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * print_to_98 - check the code.
+ * @n: Description of parameter x
+ * Description
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	printf("\n");
+}
