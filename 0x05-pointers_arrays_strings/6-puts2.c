55f9810407ebe78132ea78c403f5dbd49d4a564c
Shadihy <shadnjoroge@gmail.com>
2023-06-26 06:00:04 -0700

    I can only go one way. Ive not got a reverse gear

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,19 @@
 
 void puts2(char *str)
 {
-	int i = 0, len = 0;
+	int len, i;
 
-	while (str[i]++)
+	len = 0;
+
+	while (str[len] != '\0')
+	{
 		len++;
+	}
+
 	for (i = 0; i < len; i += 2)
+	{
 		_putchar(str[i]);
+	}
 
 	_putchar('\n');
 }
