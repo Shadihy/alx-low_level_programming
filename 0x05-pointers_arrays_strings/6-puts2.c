f0a2ba2392896a8b08ba9fd3059517358c4ca16e
Shadihy <shadnjoroge@gmail.com>
2023-06-26 05:42:53 -0700

    Half the lies they tell about me arent true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - print one char out of 2 of a string
+ * @str: char array string type
+ * Description: Print 1st char, then 3rd, then 5th, etc..
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
