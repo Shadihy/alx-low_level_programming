25a4b87f13b3f4bed5cad6ebc9523599f0767b4c
Shadihy <shadnjoroge@gmail.com>
2023-07-07 04:31:35 -0700

    prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,11 +9,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	
-	for (i = 0; i < argc; i++)
+	int a;
+
+	for (a = 0; a < argc; a++)
 	{
-		printf("%s\n", argv[i]);
+		printf("%s\n", argv[a]);
 	}
 	return (0);
 }
