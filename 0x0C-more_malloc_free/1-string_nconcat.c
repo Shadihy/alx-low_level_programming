87e0d03b07ddd6711210c9c537a57c7bfeace6e6
Shadihy <shadnjoroge@gmail.com>
2023-07-13 03:32:56 -0700

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,26 +13,26 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *con;
-    unsigned int x = n, y;
+	char *con;
+	unsigned int x = n, y;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-    for (y = 0; s1[y]; y++)
-        x++;
-    con = malloc(sizeof(char) * (x + 1));
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (y = 0; s1[y]; y++)
+		x++;
+	con = malloc(sizeof(char) * (x + 1));
 
-    if (con == NULL)
-        return (NULL);
-    
-    x = 0;
+	if (con == NULL)
+		return (NULL);
 
-    for (y = 0; s1[y]; y++)
-        con[x++] = s1[y];
-    for (y = 0; s2[y] && y < n; y++)
-        con[x++] = s2[y];
-    con[x] = '\0';
-    return (con);
+	x = 0;
+	for (y = 0; s1[y]; y++)
+		con[x++] = s1[y];
+	for (y = 0; s2[y] && y < n; y++)
+		con[x++] = s2[y];
+
+	con[x] = '\0';
+	return (con);
 }
