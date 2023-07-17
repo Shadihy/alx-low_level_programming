07832d74e645174519ceba65603fbbd250d79545
Shadihy <shadnjoroge@gmail.com>
2023-07-17 14:40:04 +0300

    dog header and definations

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,5 +21,9 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+void free_dog(dog_t *d);
 
 #endif
