219c026db5704e8f57b03125fa4087e1493454ba
Shadihy <shadnjoroge@gmail.com>
2023-07-19 13:09:09 +0300

    searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,20 +10,21 @@
  *         Otherwise - The index of the first element for which
  *                     the cmp function does not return 0.
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	
-	if (array == NULL || cmp == NULL)
+
+	if (array == NULL || size <= 0 || cmp == NULL)
 	{
 		return (-1);
 	}
+
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) != 0)
-		{
+		if (cmp(array[i]))
 			return (i);
-		}
 	}
+
 	return (-1);
 }
